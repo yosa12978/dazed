23cd2125f1f09fe82ac51f762588967adb5432ee
yosa12978 <yosa12978@gmail.com>
2024-11-18 12:29:34 +0300

    updated domain

--- internal/domain/article/repo.go
+++ internal/domain/article/repo.go
@@ -2,11 +2,15 @@ package article
 
 import (
 	"context"
+
+	"github.com/yosa12978/dazed/internal/domain/category"
 )
 
 type Repo interface {
 	GetAll(ctx context.Context, page, size int) ([]Article, error)
 	GetById(ctx context.Context, id ID) (*Article, error)
+	GetByCategory(ctx context.Context, categoryId category.ID) ([]Article, error)
+	GetUncategorized(ctx context.Context) ([]Article, error)
 	Create(ctx context.Context, article Article) error
 	Update(ctx context.Context, article Article) error
 	Delete(ctx context.Context, id ID) error
--- internal/domain/category/repo.go
+++ internal/domain/category/repo.go
@@ -1,4 +1,13 @@
 package category
 
+import "context"
+
 type Repo interface {
+	GetAll(ctx context.Context) ([]Category, error)
+	GetRoots(ctx context.Context) ([]Category, error)
+	GetById(ctx context.Context, id ID) (*Category, error)
+	GetSubcategories(ctx context.Context, parentID ParentID) ([]Category, error)
+	Create(ctx context.Context, category Category) error
+	Update(ctx context.Context, category Category) error
+	Delete(ctx context.Context, id ID) error
 }
